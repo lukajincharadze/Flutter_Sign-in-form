da578a333c7e88f7068920c61e23a3c5c1c2308c
Luka Jincharadze <lukajinchara@gmail.com>
2023-02-20 15:58:08 +0400

    finished

--- sign_in_form/lib/main.dart
+++ sign_in_form/lib/main.dart
@@ -20,42 +20,48 @@ class MyApp extends StatelessWidget {
           title: Text("Sign-In"),
         ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container (
-                child: Text("Sign-In", style: TextStyle(fontSize: 28, color: Colors.white),),
-                color: Colors.blue,
-                padding: EdgeInsets.only(left: 100, right: 100, top: 20, bottom: 20),
-              ),
-              Container (
-                width: 250,
-                padding: EdgeInsets.only(top: 20, bottom: 20),
-                child: TextField(
-                  decoration: InputDecoration(
-                      hintText: "username"
+          child: Container(
+            color: Colors.white,
+            height: 367,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container (
+                  child: Text("Sign-In", style: TextStyle(fontSize: 28, color: Colors.white),),
+                  color: Colors.blue,
+                  padding: EdgeInsets.only(left: 100, right: 100, top: 20, bottom: 20),
+                ),
+                Container (
+                  width: 250,
+                  padding: EdgeInsets.only(top: 20, bottom: 20),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        hintText: "username"
+                    ),
                   ),
                 ),
-              ),
-              Container (
-                width: 250,
-                padding: EdgeInsets.only(bottom: 20),
-                child: TextField(
-                  decoration: InputDecoration(
-                      hintText: "password"
+                Container (
+                  width: 250,
+                  padding: EdgeInsets.only(bottom: 20),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        hintText: "password"
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.only(right: 80),
-                child: Row (
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    TextButton(onPressed: () {}, child: Text("Sign In")),
-                  ],
+                Container(
+                  width: 200,
+                  margin: EdgeInsets.only(left: 50),
+                  padding: EdgeInsets.only(bottom: 90),
+                  child: Row (
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton(onPressed: () {}, child: Text("Sign In")),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
